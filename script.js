0dba77cbd10607d10daf70180bb2743e243922ea
Efiamotu-1 <musahabeebefy10@gmail.com>
2022-08-04 22:46:57 +0100

    add new bchanges

--- script.js
+++ script.js
@@ -58,3 +58,45 @@ const btnadd = document.getElementById('add');
 btnadd.addEventListener('click', () => {
   bookplus.addBook();
 });
+
+
+/*
+Activate and Deactivate Sections
+*/
+const allBooksSection = document.getElementById('all-books');
+const addNewBookSection = document.getElementById('add-new-book');
+const contactSection = document.getElementById('contact');
+const listLink = document.getElementById('list-link');
+const addLink = document.getElementById('add-link');
+const contactLink = document.getElementById('contact-link');
+
+allBooksSection.classList.add('active');
+allBooksSection.classList.remove('hide-class');
+
+function toggleVisbility(activeEle) {
+  activeEle.classList.toggle('active');
+  activeEle.classList.remove('hide-class');
+}
+
+function clearClasses(element1, element2) {
+  element1.classList.remove('active');
+  element1.classList.add('hide-class');
+
+  element2.classList.remove('active');
+  element2.classList.add('hide-class');
+}
+
+listLink.addEventListener('click', () => {
+  toggleVisbility(allBooksSection);
+  clearClasses(addNewBookSection, contactSection);
+});
+
+addLink.addEventListener('click', () => {
+  toggleVisbility(addNewBookSection);
+  clearClasses(allBooksSection, contactSection);
+});
+
+contactLink.addEventListener('click', () => {
+  toggleVisbility(contactSection);
+  clearClasses(allBooksSection, addNewBookSection);
+});
